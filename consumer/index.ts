af87c81a5c0f88789a47ff03067b3315c962192c
Sujay Ghosh <sujayg222@gmail.com>
2025-08-28 20:22:23 +0530

    Reading the consumer redis

--- consumer/index.ts
+++ consumer/index.ts
@@ -1,15 +1,17 @@
 import { createClient } from "redis";
 
 async function startRedis() {
-    const client = await createClient()
+    const client = await createClient()  // redis default port 6379
         .on("error", (err) => console.log("Redis Client Error", err))
         .connect();
 
-    const res = await client.xAdd('betteruptime:website', '*', {
-        url: "https://perplexity.com",
-        id: "1"
+    const res:any = await client.xReadGroup('india','india-1', {
+        key: 'betteruptime:website',
+        id: '>'
+    }, {
+        COUNT: 2
     });
-    console.log(res);
+    console.log(res[0].messages);
     client.destroy();
 }
 
